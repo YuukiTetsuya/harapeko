f0f340c8470cb99723c946fa3b4252356af8a2ec
YUUKI <yuukitetsuyanet@gmail.com>
2019-03-08 01:43:31 +0000

    pagetitle helper add

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # ページごとにタイトルを分けて返す
+  def full_title(page_title = '')
+    base_title = "HARAPEKO。"
+    if page_title.empty?
+      "老舗食堂専用レビューサイト" +  " | " + base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
