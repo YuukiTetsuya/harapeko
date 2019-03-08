a697b39e80e683186fc4cebc0ab5c4274e011597
YUUKI <yuukitetsuyanet@gmail.com>
2019-03-08 00:29:47 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
+  get '/term',            to: 'static_pages#term'
+  get '/privacy_policy',  to: 'static_pages#privacy_policy'
+  get '/contact',         to: 'static_pages#contact'
 
-  get 'static_pages/term'
-
-  get 'static_pages/privacy_policy'
-
-  get 'static_pages/contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "HARAPEKO。"
+  end
+
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "老舗食堂専用レビューサイト | #{@base_title}"
   end
 
   test "should get term" do
-    get static_pages_term_url
+    get term_path
     assert_response :success
+    assert_select "title", "利用規約 | #{@base_title}"
   end
 
   test "should get privacy_policy" do
-    get static_pages_privacy_policy_url
+    get privacy_policy_path
     assert_response :success
+    assert_select "title", "プライバシーポリシー | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
+    assert_select "title", "お問い合わせ | #{@base_title}"
   end
 
 end
